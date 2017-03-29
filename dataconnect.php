ccf18e80e746d634c7245f7a57b4f602a8c30451
Richard Oh <rso136@gmail.com>
2017-03-29 17:11:27 -0400

    added styling

--- dataconnect.php
+++ dataconnect.php
@@ -16,19 +16,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-//$sql = "SELECT * FROM players WHERE id = '".$q."'";
 $sql = "SELECT * FROM nycfc WHERE name = '".$q."'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 
 	while($row = $result->fetch_assoc()) {
     	echo "<h3><span id='nameA'> " . $row["name"]. "</span></h3>";
-		echo "<img src=" . "'" . $row["image"] . "'" . " height='125' width='100'>"; 
+		echo "<img class='img-rounded' id='plyrImgA' src=" . "'" . $row["image"] . "'" . " height='125' width='100'>"; 
 		echo "<h4>Position:<span id='positionA'> " . $row["position"]. "</span></h4>";
 		echo "<h4>Attack:<span id='attackA'> " . $row["attack"]. " </span></h4>";
 		echo "<h4>Defense:<span id='defenseA'> " . $row["defense"]. " </span></h4>";
 		echo "<h4>Creator:<span id='creativityA'> " . $row["creator"]. " </span></h4>";
-		echo "<button class='btn btn-primary plyrAddA' type='button' onclick='addA();'>Add to Roster</button>";
+		echo "<button class='btn btn-customA plyrAddA' type='button' onclick='addA();'>ADD TO ROSTER</button>";
+		echo "<hr id='mobileHR'>";
 	}
 } //else {
 	//echo "0 results";
@@ -40,16 +40,17 @@ if ($resultB->num_rows > 0) {
 
 	while($row = $resultB->fetch_assoc()) {
     	echo "<h3><span id='nameB'> " . $row["name"]. "</span></h3>";
-		echo "<img src=" . "'" . $row["image"] . "'" . " height='125' width='100'>"; 
+		echo "<img class='img-rounded' id='plyrImgB' src=" . "'" . $row["image"] . "'" . " height='125' width='100'>"; 
 		echo "<h4>Position:<span id='positionB'> " . $row["position"]. "</position></h4>";
 		echo "<h4>Attack:<span id='attackB'> " . $row["attack"]. " </span></h4>";
 		echo "<h4>Defense:<span id='defenseB'> " . $row["defense"]. " </span></h4>";
 		echo "<h4>Creator:<span id='creativityB'> " . $row["creator"]. " </span></h4>";
-		echo "<button class='btn btn-danger plyrAddB' type='button' onclick='addB();'>Add to Roster</button>";
+		echo "<button class='btn btn-customB plyrAddB' type='button' onclick='addB();'>ADD TO ROSTER</button>";
 	}
 } //else {
 	//echo "0 results";
 //}
+
 
 $conn->close();
 ?>
