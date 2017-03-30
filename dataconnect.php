853ad797d50d2963c1a3e5c65e60a3ed98867070
Richard Oh <rso136@gmail.com>
2017-03-29 20:31:35 -0400

    Styling overhaul

--- dataconnect.php
+++ dataconnect.php
@@ -21,14 +21,14 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 
 	while($row = $result->fetch_assoc()) {
-    	echo "<h3><span id='nameA'> " . $row["name"]. "</span></h3>";
+    	echo "<h3><b><span id='nameA'> " . $row["name"]. "</span></b></h3>";
 		echo "<img class='img-rounded' id='plyrImgA' src=" . "'" . $row["image"] . "'" . " height='125' width='100'>"; 
-		echo "<h4><span id='positionA'> " . $row["position"]. "</span></h4>";
+		echo "<h4>Position:<span id='positionA'> " . $row["position"]. "</span></h4>";
 		echo "<h4>Attack:<span id='attackA'> " . $row["attack"]. " </span></h4>";
 		echo "<h4>Defense:<span id='defenseA'> " . $row["defense"]. " </span></h4>";
 		echo "<h4>Creator:<span id='creativityA'> " . $row["creator"]. " </span></h4>";
 		echo "<button class='btn btn-customA plyrAddA' type='button' onclick='addA();'>ADD TO ROSTER</button>";
-		echo "<hr id='mobileHR'>";
+		//echo "<hr id='mobileHR'>";
 	}
 } //else {
 	//echo "0 results";
@@ -39,9 +39,9 @@ $resultB = $conn->query($sqlB);
 if ($resultB->num_rows > 0) {
 
 	while($row = $resultB->fetch_assoc()) {
-    	echo "<h3><span id='nameB'> " . $row["name"]. "</span></h3>";
+    	echo "<h3><b><span id='nameB'> " . $row["name"]. "</span></b></h3>";
 		echo "<img class='img-rounded' id='plyrImgB' src=" . "'" . $row["image"] . "'" . " height='125' width='100'>"; 
-		echo "<h4><span id='positionB'> " . $row["position"]. "</position></h4>";
+		echo "<h4>Position:<span id='positionB'> " . $row["position"]. "</position></h4>";
 		echo "<h4>Attack:<span id='attackB'> " . $row["attack"]. " </span></h4>";
 		echo "<h4>Defense:<span id='defenseB'> " . $row["defense"]. " </span></h4>";
 		echo "<h4>Creator:<span id='creativityB'> " . $row["creator"]. " </span></h4>";
